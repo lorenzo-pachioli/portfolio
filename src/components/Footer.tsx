a6ce9990b6916b21ee8080aa95365aedc25ef94e
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-07-27 17:12:55 -0300

    cv updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import linkedin from '../assets/linkedin.svg';
 import github from '../assets/github.svg';
 import email from '../assets/email.svg';
+import cv from '../assets/LorenzoPachioli.pdf';
 import './Footer.css';
 
 
@@ -17,7 +18,7 @@ export default function Footer(){
                     <li><a className='web' href='https://www.linkedin.com/in/lorenzo-pachioli/'><img src={linkedin} alt='' /></a></li>
                     <li><a className='web' href='https://github.com/lorenzo-pachioli'><img src={github} alt='' /></a></li>
                     <li><Link className='web' to='/contact' ><img src={email} alt='' /></Link></li>
-                    <li><a className='resume' href='../assests/Lorenzo Pachioli-1.pdf' download={'Lorenzo Pachioli-1.pdf'} > Resume</a></li>
+                    <li><a className='resume' href='../assests/Lorenzo Pachioli-1.pdf' download={cv} > Resume</a></li>
                 </ul>
             </div>
         </div>
